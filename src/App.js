8bff2f5fb9fb84394d63506b3924630f402db368
isaddora freitas augusto rosa <isaddorafreitasar@gmail.com>
2021-05-06 20:21:15 -0300

    map work without locations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <Map google={this.props.google} zoom={14}>
+          <Marker onClick={this.onMarkerClick}
+                  name={`Current location`} />
+          
+          <InfoWindow onClose={this.onInfoWindowClose}>
+            {/* <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div> */}
+          </InfoWindow>
+        </Map>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCXDfxEVTx9uiaCgGfber6z7r_uw41OvlM")
+})(App)
